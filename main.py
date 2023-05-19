000d862f0e412144ea6e1895f5ee9db326e4a186
Yashwanth Reddy Paakaala <75958758+kebonix17@users.noreply.github.com>
2023-05-19 15:27:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
+import io
+import random
+import os
+from fastapi.responses import StreamingResponse
+
+app = FastAPI()
+
+origins = [
+    # "http://localhost.tiangolo.com",
+    # "https://localhost.tiangolo.com",
+    # "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+async def sample():
+    return {'message': 'Hello World'}
+
+@app.get('/check')
+async def check(url : str=None):
+    #print(url)
+    return {"choice": random.choice([0,1])}
+    
